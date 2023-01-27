bf3d5bc2fb80405ab289be8a997eb67eea56d189
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-01-27 18:22:17 +0530

    attribute url

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' }); // -> title passing to index.hbs
+  res.render('index', { url: 'https://stackoverflow.com/questions/58211880/uncaught-syntaxerror-cannot-use-import-statement-outside-a-module-when-import' }); // -> title passing to index.hbs
 });
 
 module.exports = router;
